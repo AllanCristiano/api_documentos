adddfabfa1bc3d940a434e7916d6e39212035c5c
Allan Cristiano <drmundo920@gmail.com>
2026-01-28 08:31:51 -0300

    feat: new limit form

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 // main.ts (NestJS)
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express'; // 1. Importar isso
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Permitir requisições vindas do Next.js em localhost:3000
+  // 2. Aumentar o limite para JSON (ajuste '50mb' conforme sua necessidade)
+  app.use(json({ limit: '100mb' }));
+
+  // 3. Aumentar o limite para URL Encoded (necessário para formulários)
+  app.use(urlencoded({ extended: true, limit: '100mb' }));
+
+  // Permitir requisições vindas de qualquer origem
   app.enableCors({
     origin: '*',
   });
